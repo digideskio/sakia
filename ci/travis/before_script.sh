aed49f1ca270c9cf95922f52614a10cf97228305
inso <insomniak.fr@gmaiL.com>
2016-08-20 21:44:15 +0200

    Run Xfvb manually

--- ci/travis/before_script.sh
+++ ci/travis/before_script.sh
@@ -2,9 +2,10 @@
 
 if [ $TRAVIS_OS_NAME == "linux" ]
 then
-    export XVFBARGS="-screen 0 1280x1024x24 +extension RANDR +extension RENDER +extension GLX"
-    export DISPLAY=:0
-    sudo chmod a+x /etc/init.d/xvfb
-    sudo sh -e /etc/init.d/xvfb restart
+    #export XVFBARGS=":99 -screen 0 1280x1024x24 +extension RANDR +extension RENDER +extension GLX"
+    #export DISPLAY=:99.0
+    #sudo chmod a+x /etc/init.d/xvfb
+    #sudo sh -e /etc/init.d/xvfb restart
+    Xvfb :99 -screen 0 1280x960x24 +extension RANDR +extension RENDER +extension GLX &
     sleep 3
 fi
