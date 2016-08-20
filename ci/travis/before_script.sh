0c727f8e2c4106baf91eb65aac5be7f325dad0f7
inso <insomniak.fr@gmaiL.com>
2016-08-20 20:38:27 +0200

    XFVBi

--- ci/travis/before_script.sh
+++ ci/travis/before_script.sh
@@ -4,6 +4,7 @@ if [ $TRAVIS_OS_NAME == "linux" ]
 then
     export XVFBARGS="-screen 0 1280x1024x24"
     export DISPLAY=:99.0
+    sudo chmod a+x /etc/init.d/xvfb
     sudo sh -e /etc/init.d/xvfb restart
     sleep 3
 fi
