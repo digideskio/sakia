933c1016870a25165e6cc76bc55c0791d259344e
inso <insomniak.fr@gmaiL.com>
2016-08-20 20:24:21 +0200

    Try to find why it crashes

---
+++ ci/travis/before_script.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [ $TRAVIS_OS_NAME == "linux" ]
+then
+    export XVFBARGS="-screen 0 1280x1024x24"
+    export DISPLAY=:99.0
+    sudo sh -e /etc/init.d/xvfb restart
+    sleep 3
+fi
--- ci/travis/test.sh
+++ ci/travis/test.sh
@@ -2,14 +2,6 @@
 
 eval "$(pyenv init -)"
 
-if [ $TRAVIS_OS_NAME == "linux" ]
-then
-    export XVFBARGS="-screen 0 1280x1024x24"
-    export DISPLAY=:99.0
-    sudo sh -e /etc/init.d/xvfb restart
-    sleep 3
-fi
-
 cd $HOME/build/duniter/sakia
 pyenv shell $PYENV_PYTHON_VERSION
 if [ $TRAVIS_OS_NAME == "linux" ]
