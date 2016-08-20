7302aa1a51732a0f3c5b842d84702b10b9f307e9
inso <insomniak.fr@gmaiL.com>
2016-08-20 22:08:32 +0200

    Try on display 10

--- ci/travis/before_script.sh
+++ ci/travis/before_script.sh
@@ -6,6 +6,7 @@ then
     #export DISPLAY=:99.0
     #sudo chmod a+x /etc/init.d/xvfb
     #sudo sh -e /etc/init.d/xvfb restart
-    Xvfb :99 -screen 0 1280x960x24 +extension RANDR +extension RENDER +extension GLX &
+    sudo sh -e /etc/init.d/xvfb stop
+    Xvfb :10 -screen 0 1280x960x24 +extension RANDR +extension RENDER +extension GLX &
     sleep 3
 fi
