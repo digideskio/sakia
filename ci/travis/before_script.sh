ba7f26476dd2ad4b457738251fdde65c7ec713dd
inso <insomniak.fr@gmaiL.com>
2016-08-20 21:21:27 +0200

    Add GLX

--- ci/travis/before_script.sh
+++ ci/travis/before_script.sh
@@ -2,7 +2,7 @@
 
 if [ $TRAVIS_OS_NAME == "linux" ]
 then
-    export XVFBARGS="-screen 0 1280x1024x24"
+    export XVFBARGS="-screen 0 1280x1024x24 +extension RANDR +extension RENDER +extension GLX"
     export DISPLAY=:99.0
     sudo chmod a+x /etc/init.d/xvfb
     sudo sh -e /etc/init.d/xvfb restart
