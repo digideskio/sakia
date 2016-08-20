de71bc696925f333c0c745382e35df91eaa54d08
inso <insomniak.fr@gmaiL.com>
2016-08-20 22:33:29 +0200

    Add debug

--- ci/travis/before_script.sh
+++ ci/travis/before_script.sh
@@ -7,6 +7,6 @@ then
     #sudo chmod a+x /etc/init.d/xvfb
     #sudo sh -e /etc/init.d/xvfb restart
     sudo sh -e /etc/init.d/xvfb stop
-    Xvfb :10 -screen 0 1280x960x24 +extension RANDR +extension RENDER +extension GLX &
+    LIBGL_DEBUG=1 Xvfb :10 -screen 0 1280x960x24 +extension RANDR +extension RENDER +extension GLX &
     sleep 3
 fi
