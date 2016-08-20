228ffacc5b6a8bedaddb9930a815d503419f663e
inso <insomniak.fr@gmaiL.com>
2016-08-20 23:01:52 +0200

    Try without coverage

--- ci/travis/test.sh
+++ ci/travis/test.sh
@@ -7,7 +7,8 @@ pyenv shell $PYENV_PYTHON_VERSION
 if [ $TRAVIS_OS_NAME == "linux" ]
 then
     export DISPLAY=:10.0
-    coverage run --source=sakia.core,sakia.gui,sakia.models setup.py test
+    #coverage run --source=sakia.core,sakia.gui,sakia.models setup.py test
+    python setup.py test
 else
     python setup.py test
 fi
